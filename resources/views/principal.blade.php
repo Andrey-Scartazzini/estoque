4dd2dfa06f1be85db584b4ecf8fe25f1c750956d
Andrey-Scartazzini <andreyfeldmann@gmail.com>
2018-07-20 17:00:05 -0300

    fim do dia (20/07)
    Pag 110

---
+++ resources/views/principal.blade.php
@@ -0,0 +1,12 @@
+
+<html>
+<head>
+    <link href="/css/bootstrap.css" rel="stylesheet">
+    <title>Controle de estoque</title>
+</head>
+<body>
+<div class="container">
+    @yield('conteudo')
+</div>
+</body>
+</html>
