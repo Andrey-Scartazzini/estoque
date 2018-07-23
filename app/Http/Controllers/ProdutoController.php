19f9f1eb0544e0795a919f0ba97176897b50377e
Andrey-Scartazzini <andreyfeldmann@gmail.com>
2018-07-23 12:04:46 -0300

    CAP 10
    Oag (154)

--- app/Http/Controllers/ProdutoController.php
+++ app/Http/Controllers/ProdutoController.php
@@ -30,9 +30,14 @@ class ProdutoController extends Controller{
         $descricao = Request::input('descricao');
         $valor = Request::input('valor');
         $quantidade = Request::input('quantidade');
+
         DB::insert('insert into produtos (nome, descricao, valor, quantidade) values (?,?,?,?)',
             array($nome, $descricao, $valor, $quantidade));
 
-        return view('produto.adicionado')->with('nome', $nome);
+        return redirect()->action('ProdutoController@lista')->withInput(Request::only('nome'));
+    }
+    public function listaJson(){
+        $produtos= DB::select('select * from produtos');
+        return response()->json($produtos);
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 Route::get('/produtos', 'ProdutoController@lista');
+Route::get('/produtos/json', 'ProdutoController@listaJson');
 Route::get('/produtos/novo', 'ProdutoController@novo');
 Route::post('/produtos/adiciona', 'ProdutoController@adiciona');
 Route::get('/produtos/mostra/{id}','ProdutoController@mostra')->where('id', '[0-9]+');
